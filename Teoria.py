031f9a3260fd2903bf94a76d0d6f66e73f48d394
Arthur <arthur.1968@hotmail.com>
2022-01-15 13:01:23 -0300

    teoria primeiro commit

---
+++ Teoria.py
@@ -0,0 +1,31 @@
+'''
+
+PYTHON - BASICO
+
+1 - Introdução
+
+2 - variaveis
+
+3 - Strings
+
+4 - numeros
+
+5 - listas
+
+6 - tuplas
+
+7 - dicionarios
+
+8 - estrutura de decisao
+
+9 - estrutura de repetição
+
+10 - funções
+
+
+PYTHON POO
+
+
+
+
+'''
---
+++ pratica.py
@@ -0,0 +1,19 @@
+
+
+
+'''
+
+
+while True:
+    a = int(input('digite um numero:'))
+
+    b = int(input('digite um numero:'))
+
+    def soma(x,y):
+        return x+y
+
+    som =soma(a,b)
+
+    print(f'a soma é:{som}')
+'''
+
